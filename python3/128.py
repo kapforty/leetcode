d8a7babfda07b2894a39d502d74a6970e2ef61bd
jordan peranginangin <49621451+kapforty@users.noreply.github.com>
2023-06-25 14:11:47 -0700

    128.py

---
+++ python3/128.py
@@ -0,0 +1,14 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        numSet = set(nums)
+        longest = 0
+        
+        for n in nums:
+            if n-1 in numSet:
+                continue
+            length = 1
+            while n + length in numSet:
+                length += 1
+            longest = max(longest, length)
+
+        return longest
