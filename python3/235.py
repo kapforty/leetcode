24d01f726706355409c60b0858f4b3a4d63a1599
jordan peranginangin <49621451+kapforty@users.noreply.github.com>
2023-07-11 22:21:56 -0700

    Create 235.py

---
+++ python3/235.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        # --- O(N LOG N) RUNTIME, O(1) SPACE COMPLEXITY ---
+        while root:
+            if p.val < root.val and q.val < root.val:
+                root = root.left
+            elif p.val > root.val and q.val > root.val:
+                root = root.right
+            else:
+                return root
+
+        # --- O(N LOG N) RUNTIME, O(N LOG N) SPACE COMPLEXITY ---
+        # if p.val < root.val and q.val < root.val:
+        #     return self.lowestCommonAncestor(root.left, p, q)
+        # elif p.val > root.val and q.val > root.val:
+        #     return self.lowestCommonAncestor(root.right, p, q)
+        # return root
