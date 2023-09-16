487e8ea78272b5440d44e684e42b9be86ebec5bd
jordan peranginangin <49621451+kapforty@users.noreply.github.com>
2023-09-16 15:18:02 -0700

    Create 283.py

---
+++ python3/283.py
@@ -0,0 +1,15 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        ptr = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[ptr] = nums[i]
+                ptr += 1
+        
+        for i in range(ptr, len(nums), 1):
+            nums[i] = 0
+
+        return nums
