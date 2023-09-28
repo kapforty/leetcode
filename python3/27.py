df6b22552edf358c83b01afb9248f28139fae89f
jordan peranginangin <49621451+kapforty@users.noreply.github.com>
2023-09-28 06:56:45 -0700

    Create 27.py

--- python3/27.py
+++ python3/27.py
@@ -1,14 +1,23 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        l, r = 0, len(nums) - 1
+        idx = 0
+        for num in nums:
+            if num != val:
+                nums[idx] = num
+                idx += 1
+        return idx
 
-        while l <= r:
-            if nums[r] == val:
-                r -= 1
-                continue
-            if nums[l] == val:
-                nums[l], nums[r] = nums[r], nums[l]
-                r -= 1
-            l += 1
+# class Solution:
+#     def removeElement(self, nums: List[int], val: int) -> int:
+#         l, r = 0, len(nums) - 1
 
-        return r + 1
+#         while l <= r:
+#             if nums[r] == val:
+#                 r -= 1
+#                 continue
+#             if nums[l] == val:
+#                 nums[l], nums[r] = nums[r], nums[l]
+#                 r -= 1
+#             l += 1
+
+#         return r + 1
