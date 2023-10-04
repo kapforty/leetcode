059977414723fcca119317240fb1735253f22c9f
jordan peranginangin <49621451+kapforty@users.noreply.github.com>
2023-10-04 00:03:06 -0700

    Create 706.py

---
+++ python3/706.py
@@ -0,0 +1,13 @@
+class MyHashMap:
+
+    def __init__(self):
+        self.hashMap = [-1] * 1000001
+
+    def put(self, key: int, value: int) -> None:
+        self.hashMap[key] = value
+
+    def get(self, key: int) -> int:
+        return self.hashMap[key]
+
+    def remove(self, key: int) -> None:
+        self.hashMap[key] = -1
