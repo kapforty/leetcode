f25fe9c119bd13e1e4c120032f20c58d215bfea4
jordan peranginangin <49621451+kapforty@users.noreply.github.com>
2023-10-18 17:43:43 -0700

    Create 844.py

---
+++ python3/844.py
@@ -0,0 +1,18 @@
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        stack1, stack2 = [], []
+
+        for char in s:
+            if char == "#":
+                if stack1:
+                    stack1.pop()
+            else:
+                stack1.append(char)
+        for char in t:
+            if char == "#":
+                if stack2:
+                    stack2.pop()
+            else:
+                stack2.append(char)
+
+        return stack1 == stack2
