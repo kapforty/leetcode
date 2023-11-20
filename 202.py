1c02c969cc9f65a83050c2a78571c2fd73697d41
jordan peranginangin <49621451+kapforty@users.noreply.github.com>
2023-11-20 02:11:23 -0800

    Delete 202.py

--- 202.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def isHappy(self, n: int) -> bool:
-        visited = set()
-        while n != 1:
-            if n in visited:
-                return False
-            visited.add(n)
-            n = sum([int(i) ** 2 for i in str(n)])
-        return True
