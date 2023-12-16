5ff70dc62d936d156ae5b4f90230f435b6209969
jordan peranginangin <49621451+kapforty@users.noreply.github.com>
2023-12-16 10:23:52 -0800

    Update 128.py

--- python3/128.py
+++ python3/128.py
@@ -1,5 +1,18 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
+        nums = set(nums)
+        res = 0
+        for num in nums:
+            if num - 1 in nums:
+                continue
+            length = 1
+            while num + length in nums:
+                length += 1
+            res = max(res, length)
+        return res
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
         numSet = set(nums)
         longest = 0
         
