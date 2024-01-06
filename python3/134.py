ed8320e286ae1e3d827ee43fe8a2c8c3b3e6e08e
jordan peranginangin <49621451+kapforty@users.noreply.github.com>
2024-01-05 18:28:22 -0800

    Update 134.py

--- python3/134.py
+++ python3/134.py
@@ -1,5 +1,19 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        if sum(gas) < sum(cost):
+            return -1
+
+        res, curr = -1, 0
+        for i in range(len(gas)):
+            ev = gas[i] - cost[i]
+            if ev >= curr + ev:
+                res = i
+            curr = max(curr + ev, ev)
+
+        return res
+
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
         diff = []
         res, currSum = 0, 0
 
