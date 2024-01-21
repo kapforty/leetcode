894f2b0b2e9525d5f2db649a9fb5c38784534fc7
jordan peranginangin <49621451+kapforty@users.noreply.github.com>
2024-01-21 01:16:23 -0800

    Create 1561.py

--- python3/1561.py
+++ python3/1561.py
@@ -1,5 +1,14 @@
 class Solution:
     def maxCoins(self, piles: List[int]) -> int:
+        res, ptr = 0, len(piles)
+        piles.sort()
+        for i in range(len(piles)//3):
+            ptr -= 2
+            res += piles[ptr] 
+        return res
+
+class Solution:
+    def maxCoins(self, piles: List[int]) -> int:
         res = 0
         l, r = 0, len(piles)-2
         piles.sort()
