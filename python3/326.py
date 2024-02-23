9ec26c11ec4a4fc8863c3e4c9db28d6dd8033c35
jordan peranginangin <49621451+kapforty@users.noreply.github.com>
2024-02-22 23:13:52 -0800

    Create 326.py

---
+++ python3/326.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        curr = 1
+        while curr <= n:
+            if curr == n:
+                return True
+            curr *= 3
+        return False
