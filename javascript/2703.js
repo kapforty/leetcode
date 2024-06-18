3a2d2a9443a47276557a11eafa4614f86707b170
kapforty <jordanangin@gmail.com>
2024-06-17 22:41:38 -0700

    created javascript folder

--- javascript/2703.js
+++ javascript/2703.js
@@ -2,8 +2,8 @@
  * @param {...(null|boolean|number|string|Array|Object)} args
  * @return {number}
  */
-var argumentsLength = function(...args) {
-    return args.length;
+var argumentsLength = function (...args) {
+  return args.length;
 };
 
 /**
