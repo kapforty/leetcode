23331375e27b85a50c93b6da4318a0a882642f7a
jordan peranginangin <49621451+kapforty@users.noreply.github.com>
2024-07-09 22:57:48 -0700

    Create 1598.py

---
+++ python3/1598.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        res = 0
+        for log in logs:
+            if log == "../":
+                if res > 0:
+                    res -= 1
+            elif log == "./":
+                continue
+            else:
+                res += 1
+        return res
