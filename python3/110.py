55d8e60516218ce0cc097cab4953ae444b2a95b6
jordan peranginangin <jordan.peranginangin@netapp.com>
2024-07-22 00:47:09 -0700

    Create 110.py

---
+++ python3/110.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        balanced = True
+        def dfs(node):
+            nonlocal balanced
+            if not node or not balanced:
+                return 0
+            left, right = dfs(node.left), dfs(node.right)
+            if abs(left - right) > 1:
+                balanced = False
+            return max(left, right) + 1
+        dfs(root)
+        return balanced
+            
