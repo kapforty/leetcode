8d4e56a923754d15baae22cc216429b81cafb0ee
jordan peranginangin <jordanangin@gmail.com>
2024-08-14 17:08:35 -0700

    Create 860.py

---
+++ python3/860.py
@@ -0,0 +1,20 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        fives = tens = 0
+        for bill in bills:
+            if bill == 5:
+                fives += 1
+            elif bill == 10:
+                if fives < 1:
+                    return False
+                tens += 1
+                fives -= 1
+            else:
+                if tens > 0 and fives > 0:
+                    tens -= 1
+                    fives -= 1
+                elif fives >= 3:
+                    fives -= 3
+                else:
+                    return False
+        return True
