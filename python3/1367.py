c3da9a19657a356207a229e73f643d7de1e4a9ad
jordan peranginangin <jordanangin@gmail.com>
2024-09-06 20:41:43 -0700

    Create 1367.py

---
+++ python3/1367.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubPath(self, head: Optional[ListNode], root: Optional[TreeNode]) -> bool:
+        if not root:
+            return False
+        if self.dfs(head, root):
+            return True
+        return self.isSubPath(head, root.left) or self.isSubPath(head, root.right)
+        
+    def dfs(self, head, root):
+        if not head:
+            return True
+        if not root or head.val != root.val:
+            return False
+        return self.dfs(head.next, root.left) or self.dfs(head.next, root.right)
