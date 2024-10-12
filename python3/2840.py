47f51bf37350c89667ab08d06c19fb0e7b2465aa
jordan peranginangin <jordanangin@gmail.com>
2024-10-11 21:16:08 -0700

    Create 2840.py

---
+++ python3/2840.py
@@ -0,0 +1,11 @@
+class Solution:
+    def checkStrings(self, s1: str, s2: str) -> bool:
+        even, odd, target = [0 for _ in range(26)], [0 for _ in range(26)], [0 for _ in range(26)]
+        for i in range(len(s1)):
+            if i % 2 == 0:
+                even[ord(s1[i]) - ord('a')] -= 1
+                even[ord(s2[i]) - ord('a')] += 1
+            else:
+                odd[ord(s1[i]) - ord('a')] -= 1
+                odd[ord(s2[i]) - ord('a')] += 1
+        return even == odd == target
