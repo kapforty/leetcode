4e8e780ab60627faf332b585d46cb80860cb9a53
jordan peranginangin <jordanangin@gmail.com>
2025-04-03 21:51:54 -0700

    Create 865.py

---
+++ python3/865.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def subtreeWithAllDeepest(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        # calculate heights of left/right subtrees from each node
+        left, right = defaultdict(int), defaultdict(int)
+        def dfs(node):
+            if not node:
+                return 0
+            left[node.val], right[node.val] = dfs(node.left), dfs(node.right)
+            return max(left[node.val], right[node.val]) + 1
+        dfs(root)
+
+        # traverse down binary tree until left/right subtrees have equal heights
+        curr = root
+        while left[curr.val] != right[curr.val]:
+            curr = curr.left if left[curr.val] > right[curr.val] else curr.right
+        return curr
