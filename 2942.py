5b56627c2399792582a71eb442187b31589baa68
jordan peranginangin <jordan.peranginangin@netapp.com>
2025-06-04 21:38:41 -0700

    Reorganize files

--- 2942.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def findWordsContaining(self, words: List[str], x: str) -> List[int]:
-        res = []
-        for i, word in enumerate(words):
-            if x in word:
-                res.append(i)
-        return res
